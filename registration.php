c73bdd1f530499f2b49a57308b6db5a665e0058c
AkiP4 <67232776+AkiP4@users.noreply.github.com>
2021-03-24 10:16:27 +0200

    Added the base of the registration page

---
+++ registration.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<HTML>
+
+<head>
+</head>
+    <title>User registration</title>
+    <link rel="stylesheet" href="style.css">
+<body>
+    <div class="header">
+        <h2>Register</h2>
+    </div>
+</body>
+
+</HTML>
